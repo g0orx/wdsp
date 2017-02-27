71aa9df228e40dc64b22aa683b910eefbca215b4
Jae Stutzman <jaebird@gmail.com>
2017-02-26 20:21:10 -0600

    added wdsp_beginthread

---
+++ comm.c
@@ -0,0 +1,31 @@
+/*  comm.c
+
+This file is part of a program that implements a Software-Defined Radio.
+
+Copyright (C) 2017  Warren Pratt, NR0V, Jae Stutzman, K5JAE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+*/
+
+#include "comm.h"
+
+#ifdef _WINDOWS_
+uintptr_t __cdecl wdsp_beginthread(void(__cdecl *start_address)(void *), unsigned stack_size, void *arglist, char *name)
+{
+	return _beginthread(start_address, stack_size, arglist);
+}
+#endif
