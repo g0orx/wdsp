7d831bee431c2e15b5e21bf541e9ab7dc1a617b2
DL1YCF <dl1ycf@darc.de>
2021-12-31 15:13:06 +0100

    Forgot to commit one fix.

--- comm.c
+++ comm.c
@@ -1,4 +1,4 @@
-*  comm.c
+/*  comm.c
 
 This file is part of a program that implements a Software-Defined Radio.
 
--- utilities.c
+++ utilities.c
@@ -266,7 +266,7 @@ void __cdecl CalccPrintSamples (void *pargs)
 {
 	int i;
 	double env_tx, env_rx;
-	int channel = (int)pargs;
+	int channel = (int)(uintptr_t)pargs;
 	CALCC a = txa[channel].calcc.p;
 	FILE* file = fopen("samples.txt", "w");
 	fprintf (file, "\n");
@@ -285,7 +285,7 @@ void __cdecl CalccPrintSamples (void *pargs)
 
 void doCalccPrintSamples(int channel)
 {	// no sample buffering - use in single cal mode
-	_beginthread(CalccPrintSamples, 0, (void *)channel);
+	_beginthread(CalccPrintSamples, 0, (void *)(uintptr_t)channel);
 }
 
 void print_anb_parms (const char* filename, ANB a)
